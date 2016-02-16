a48f8429c3a09a4de2c75c21ddae8f264f73954e
Kyle Ladd <kladd6@gmail.com>
2016-02-15 19:10:07 -0500

    Basictable for responsiveness

--- asset/js/schedulizer.js
+++ asset/js/schedulizer.js
@@ -230,6 +230,7 @@ $.extend(mscSchedulizer, {
         $.getJSON(mscSchedulizer.api_host + "/courses/?department_code=" + department + "&include_objects=1&semester="+mscSchedulizer.semester.TermCode, function(results){
             var output = mscSchedulizer.detailedCoursesOutput(results);
             $("#"+mscSchedulizer.html_elements.department_class_list).html(output);
+            $('.course_details').basictable();
         })
         .fail(function() {
             $("#"+mscSchedulizer.html_elements.department_class_list).html("<p>Unable to load course listings.</p>");
@@ -323,7 +324,6 @@ $.extend(mscSchedulizer, {
         });
     },
     groupMeetings:function(meetings){
-        console.log(meetings);
         groupedMeetings = [];
         for (var m in meetings) {
             var meeting = meetings[m];
