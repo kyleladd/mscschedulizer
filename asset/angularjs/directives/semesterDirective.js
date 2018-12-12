b4e1f507fe10cf9758d8c2a6c13da33abd51ffa4
Kyle Ladd <kladd6@gmail.com>
2018-12-12 18:19:56 -0500

    ui.select2

--- asset/angularjs/directives/semesterDirective.js
+++ asset/angularjs/directives/semesterDirective.js
@@ -1,20 +1,38 @@
 // app.factory('settings', ['$http', function($http) {
 //     return $http.get('/api/public/settings/get')
 // }]);
-define(['angular', 'angular-ui-select'], function (angular,uiselect) {
-    angular.module("semesterDirective", [])
+define(['angular', 'angular-ui-select', 'ngSanitize'], function (angular,uiselect) {
+    angular.module("semesterDirective", ['ui.select','ngSanitize'])
     .component("semesterSelector",{
         // template: '<select ng-model="$ctrl.value" ng-change="$ctrl.changedValue($ctrl.value)"><option ng-repeat="option in $ctrl.options" value="{{option.TermCode}}">{{option.Description}}</option></select>',
         // template: '<select ng-model="$ctrl.semester"><option ng-repeat="option in $ctrl.semesters" value="{{option.TermCode}}">{{option.Description}}</option></select>',
         // template: '<input ng-model="$ctrl.semester" />',
         // template: 'inner: <input type="text" ng-model="$ctrl.semester">',
         // template: '<select ng-model="$ctrl.value" ng-change="{{$ctrl.change({value:$ctrl.value})}}"><option ng-repeat="option in $ctrl.options" value="{{option.TermCode}}">{{option.Description}}</option></select>',
-        template: '<select ng-model="$ctrl.value" ng-change="{{$ctrl.changedValue($ctrl.value)}}"><option ng-repeat="option in $ctrl.semesters" value="{{option.TermCode}}">{{option.Description}}</option></select>',
+        template: '<select ng-model="$ctrl.value" ng-change="{{$ctrl.changedValue($ctrl.value)}}">\
+                    <option ng-repeat="option in $ctrl.semesters" value="{{option.TermCode}}">{{option.Description}}</option>\
+                   </select>\
+                    <ui-select ng-model="$ctrl.value" id="ptype" theme="bootstrap" title="Select a Semester" ng-change="{{$ctrl.changedValue($ctrl.value)}}">\
+                        <ui-select-match placeholder="Select a Semester">{{$select.selected.Description}}</ui-select-match>\
+                        <ui-select-choices repeat="semester.TermCode as semester in $ctrl.semesters | filter: $select.search">\
+                            <div ng-bind-html="semester.Description | highlight: $select.search"></div>\
+                        </ui-select-choices>\
+                    </ui-select>',
         bindings: { 
             semester: '<',
             semesters: '<',
             change:'&'
         },
+      //   <ui-select ng-model="$ctrl.value"\
+      //            theme="bootstrap"\
+      //            ng-disabled="$ctrl.disabled"\
+      //            reset-search-input="false"\
+      //            title="Choose an address" ng-change="{{$ctrl.changedValue($ctrl.value)}}">\
+      //   <ui-select-match placeholder="Select a Semester...">{{$select.selected.TermCode}}</ui-select-match>\
+      //   <ui-select-choices repeat="a_semester in $ctrl.semesters track by a_semester.TermCode">\
+      //     <div ng-bind-html="a_semester.TermCode | highlight: $select.search"></div>\
+      //   </ui-select-choices>\
+      // </ui-select>
         // bindings: { connection: '=' },
         controllerAs: '$ctrl',
         controller: function($scope) {
@@ -35,6 +53,9 @@ define(['angular', 'angular-ui-select'], function (angular,uiselect) {
                 // $ctrl.options = $ctrl.semesters;
             };
             $ctrl.changedValue = function(value){
+                // if(value){
+                //     $ctrl.value = value.TermCode;
+                // }
                 console.log("CHANGED value", value, $ctrl.value );
                 $ctrl.change({value:$ctrl.value});
                 // $ctrl.semester = value;
