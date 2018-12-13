e20d17491739e513568ee94993283c4397130926
Kyle Ladd <kladd6@gmail.com>
2018-12-13 13:44:19 -0500

    Course Listings component - should make the sidebar a component to deduplicate code between select and course listings

--- asset/angularjs/main.js
+++ asset/angularjs/main.js
@@ -44,8 +44,7 @@ define([
           name: 'course-listings',
           // url: '/view1/{bowlingID}',
           url: "/course-listings",
-          templateUrl: '/templates/course-listings.html',
-          controller: 'CourseListingsCtrl'
+          component: 'courseListingsComponent'
         });
        $stateProvider.state({
           name: 'visual-filter',
@@ -74,7 +73,6 @@ define([
       controller: function($scope, userService){
         var $ctrl = this;
         $ctrl.$onInit = function () {
-          // $ctrl.semesters = [];
           $ctrl.semester = userService.get_semester();
           $ctrl.department = userService.get_department();
         };
@@ -82,7 +80,6 @@ define([
         console.log("userService", userService);
         $ctrl.semesterChanged = function(value){
           console.log('Semester changed to ' + value);
-          // $ctrl.semester = value;
           userService.set_semester(value);
           console.log("going to load the departments component value");
         };
@@ -91,51 +88,50 @@ define([
           userService.set_department(value);
           console.log("going to load the departments component value");
         };
-        // $scope.$watch(angular.bind($ctrl, function () {
-        //   return $ctrl.semester;
-        // }), function (newVal) {
-        //   console.log('Semester changed to ' + newVal);
-        //   userService.set_semester(newVal);
-        // });
-        // $scope.$watch(function () {
-        //    return $ctrl.semester;
-        // },function(value){
-        //   console.log('Semester changed to ' + value);
-        //   userService.set_semester(value);
-        // });
-        // $scope.$watch('semester', function(newValue, oldValue){
-        //   console.log("watch semester",newValue);
-        //   userService.set_semester(newValue);
-        // }, true);
         $scope.$on('semester:set', function(event, data){
           console.log("setting semester based on event", data);
-            // $scope.$apply(function () {
-              $ctrl.semester = data;
-            // });
+          $ctrl.semester = data;
         });
         $scope.$on('department:set', function(event, data){
           console.log("setting department based on event", data);
-            // $scope.$apply(function () {
-              $ctrl.department = data;
-            // });
+          $ctrl.department = data;
         });
       }
     });
-    // app.controller('SelectClassesCtrl', ['$scope', 'userService',function ($scope, userService) {
-    //   console.log("SelectClassesCtrl");
-    //   console.log("userService", userService);
-    //   $scope.semester = userService.get_semester();
-    //   $scope.$watch('semester', function(newValue, oldValue){
-    //     console.log("watch semester",newValue);
-    //       userService.set_semester(newValue);
-    //   });
-    // }]);
     app.controller('GenerateCtrl', ['$scope',function ($scope) {
       console.log("GenerateCtrl");
     }]);
-    app.controller('CourseListingsCtrl', ['$scope',function ($scope) {
-      console.log("CourseListingsCtrl");
-    }]);
+    app.component("courseListingsComponent",{
+      templateUrl: '/templates/course-listings.html',
+      controllerAs: "$ctrl",
+      controller: function($scope, userService){
+        var $ctrl = this;
+        $ctrl.$onInit = function () {
+          $ctrl.semester = userService.get_semester();
+          $ctrl.department = userService.get_department();
+        };
+        console.log("SelectClassesCtrl");
+        console.log("userService", userService);
+        $ctrl.semesterChanged = function(value){
+          console.log('Semester changed to ' + value);
+          userService.set_semester(value);
+          console.log("going to load the departments component value");
+        };
+        $ctrl.departmentChanged = function(value){
+          console.log('department changed to ' + value);
+          userService.set_department(value);
+          console.log("going to load the departments component value");
+        };
+        $scope.$on('semester:set', function(event, data){
+          console.log("setting semester based on event", data);
+          $ctrl.semester = data;
+        });
+        $scope.$on('department:set', function(event, data){
+          console.log("setting department based on event", data);
+          $ctrl.department = data;
+        });
+      }
+    });
     app.controller('VisualFilterCtrl', ['$scope',function ($scope) {
       console.log("VisualFilterCtrl");
     }]);
