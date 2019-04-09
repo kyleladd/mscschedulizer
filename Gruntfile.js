83e9341ddb5a25b6fcb182bf0ac05ca4b376a648
Kyle Ladd <kladd6@gmail.com>
2019-04-08 23:26:15 -0400

    remove some more

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,6 @@
 module.exports = function(grunt) {
-  var target = grunt.option('target') || 'dev';
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-     options: {
-       banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-     },
-     build: {
-       src: 'asset/js/schedulizer_bundle.js',
-       dest: 'asset/js/schedulizer_bundle.min.js'
-     }
-   },
-    browserify: {
-      dist: {
-        files: {
-          'asset/js/schedulizer_bundle.js': ['asset/js/bundleme.js']
-        }
-      }
-    },
+  // var target = grunt.option('target') || 'dev';
+  grunt.initConfig(
     cssmin: {
       target: {
         files: [{
@@ -47,48 +30,13 @@ module.exports = function(grunt) {
           event:['all']
         },
       },
-      scripts: {
-        files: 'asset/js/*',
-        tasks: ['browserify','copy:browserifybundle'],
-        options: {
-          spawn:false,
-          event:['all']
-        },
-      },
     },
-    copy: {
-      main: {
-        expand: true, src: ['asset/js/config.example.js'], dest: '',
-        // Copy if file does not exist.
-        filter: function (filepath) {
-            // Return false if the file exists.
-            return !(grunt.file.exists(filepath.replace('.example','')));
-        },
-        rename: function(dest, src) {
-          return dest + src.replace('.example','');
-        }
-      },
-      browserifybundle: {
-        expand: true, src: ['asset/js/schedulizer_bundle.js'], dest: '',
-        rename: function(dest, src) {
-          return dest + src.replace('.js','.min.js');
-        }
-      }
-    }
   });
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-bower-task');
-  var tasks = ['copy:main','browserify','cssmin'];
-  if(target==="prod"){
-    tasks.push("uglify");
-  }
-  else{
-    tasks.push("copy:browserifybundle"); 
-  }
+  var tasks = ['copy:main','cssmin'];
   grunt.registerTask('default', tasks);
-  grunt.registerTask('dev', ['default','watch']);
+  // grunt.registerTask('dev', ['default','watch']);
 };
