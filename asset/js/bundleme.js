5ac3af0ec037ac48a8378cbbe766f45f2934ea2f
Kyle Ladd <kladd6@gmail.com>
2019-04-08 23:09:37 -0400

    Remove some more

--- asset/js/bundleme.js
+++
@@ -1,9 +0,0 @@
-var mscSchedulizer_config = require('./config.js');
-var node_generic_functions = require('node_generic_functions');
-var mscSchedulizer = require('./schedulizer.js');
-var Combinatorics = require('js-combinatorics');
-window.mscSchedulizer = mscSchedulizer;
-window.mscSchedulizer_config = mscSchedulizer_config;
-window.node_generic_functions = node_generic_functions;
-window.mscSchedulizer = mscSchedulizer;
-window.Combinatorics = Combinatorics;
