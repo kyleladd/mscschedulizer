ad94e5ac7661f4e7ae6b9c8cdd338b3ee0ffa687
Kyle Ladd <kladd6@gmail.com>
2019-04-09 00:45:47 -0400

    More build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   // var target = grunt.option('target') || 'dev';
-  grunt.initConfig(
+  grunt.initConfig({
     cssmin: {
       target: {
         files: [{
@@ -21,6 +21,43 @@ module.exports = function(grunt) {
         }]
       }
     },
+    move: {
+      main: {
+        options: {
+          ignoreMissing: true
+        },
+        files: [
+        {
+          src: 'node_modules/flatted/index.js',
+          dest: 'modules/flatted/index.js'
+        },
+        {
+          src: 'node_modules/node_generic_functions/genericfunctions.js',
+          dest: 'modules/node_generic_functions/genericfunctions.js'
+        },
+        {
+          src: 'node_modules/js-combinatorics/combinatorics.js',
+          dest: 'modules/js-combinatorics/combinatorics.js'
+        },
+        {
+          src: 'bower_components/basictable/jquery.basictable.min.js',
+          dest: 'modules/basictable/jquery.basictable.min.js'
+        },
+        {
+          src: 'bower_components/basictable/basictable.css',
+          dest: 'modules/basictable/basictable.css'
+        },
+        {
+          src: 'bower_components/fullcalendar/dist/fullcalendar.min.js',
+          dest: 'modules/fullcalendar/fullcalendar.min.js'
+        },
+        {
+          src: 'bower_components/fullcalendar/dist/fullcalendar.min.css',
+          dest: 'modules/fullcalendar/fullcalendar.min.css'
+        }
+      ]
+      }
+    },
     watch: {
       css: {
         files: 'css/*',
@@ -32,11 +69,11 @@ module.exports = function(grunt) {
       },
     },
   });
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-bower-task');
-  var tasks = ['copy:main','cssmin'];
+  grunt.loadNpmTasks('grunt-move');
+  var tasks = ['move','cssmin'];
   grunt.registerTask('default', tasks);
-  // grunt.registerTask('dev', ['default','watch']);
+  grunt.registerTask('dev', ['default','watch']);
 };
